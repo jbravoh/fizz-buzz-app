8c200dd344a8b107aea46a8c64e5aba93eb1e5c7
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-08-26 09:56:07 +0100

    Update test result

--- src/utils/asFizzBuzz.test.ts
+++ src/utils/asFizzBuzz.test.ts
@@ -3,5 +3,5 @@ import { asFizzBuzz } from "./asFizzBuzz";
 test("greet returns a string, greeting the passed name", () => {
   expect(asFizzBuzz(15)).toBe("FizzBuzz");
   expect(asFizzBuzz(3)).toBe("Fizz");
-  expect(asFizzBuzz(11)).toBe(11);
+  expect(asFizzBuzz(11)).toBe("11");
 });
